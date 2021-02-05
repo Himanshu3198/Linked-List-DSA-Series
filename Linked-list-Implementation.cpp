c69d153b0ce30d8b104799ac42f68360a56893d8
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-05 17:27:34 +0530

    Update Linked-list-Implementation.cpp

--- Linked-list-Implementation.cpp
+++ Linked-list-Implementation.cpp
@@ -8,6 +8,9 @@ struct Node {
    struct Node *next;
 };
 struct Node* head = NULL;
+
+
+
 void insert(int new_data) {
    // struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
    Node* new_node=new Node();
@@ -15,6 +18,65 @@ void insert(int new_data) {
    new_node->next = head;
    head = new_node;
 }
+
+
+
+
+void insertAtEnd(int data){
+
+   Node * new_node=new Node();
+    struct Node* temp;
+
+
+
+    
+     new_node->data=data;
+    new_node->next=NULL;
+    temp=head;
+    while(temp->next!=NULL){
+       temp=temp->next;
+    }
+   
+      temp->next=new_node;
+
+
+}
+
+
+
+
+void insertAtPosition(int data,int pos){
+
+   int count=0;
+   Node * new_node= new Node();
+   struct Node* temp;
+       struct Node* prevnode;
+     struct Node*   nextnode;
+   new_node->data=data;
+   new_node->next=NULL;
+
+   temp=head;
+   if(pos>1){
+   while(count<=pos-2){
+       
+        temp=temp->next;
+        count++;
+   }
+   prevnode=temp;
+   nextnode=temp->next;;
+
+   prevnode->next=new_node;
+   new_node->next=nextnode;
+   }
+   else{
+      new_node->next=head;
+      head=new_node;
+   }
+
+}
+
+
+
 void display() {
    struct Node* ptr;
    ptr = head;
@@ -29,7 +91,14 @@ int main() {
    insert(7);
    insert(2);
    insert(9);
+     insert(19);
    cout<<"The linked list is: ";
+
+   // insertAtEnd(40);
+   //   insertAtEnd(50);
+   //  insert(29);
+    insertAtPosition(60,6);
+
    display();
    return 0;
 }
