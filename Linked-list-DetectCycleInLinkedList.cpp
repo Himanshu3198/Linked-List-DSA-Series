1c5d71b89c377e1db87efaa900ee12407f9a2316
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-18 19:56:07 +0530

    Add files via upload

---
+++ Linked-list-DetectCycleInLinkedList.cpp
@@ -0,0 +1,67 @@
+//  Reverse a Linked List using c++
+//  Time Complexity O(n)  where n is no of nodes in linked list
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+
+    struct Node *next;
+    int data;
+};
+
+void insert(Node *head, int value)
+{
+
+    struct Node *newnode = new Node();
+
+    newnode->data = value;
+    newnode->next = head;
+    head = newnode;
+}
+
+// void createLoop(Node *head)
+// {
+//     head->next->next->next->next = head;
+// }
+
+bool detectCycle(Node *head)
+{
+    Node *fast = head;
+    Node *slow = head;
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    Node *head = NULL;
+
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    insert(head, 4);
+    insert(head, 5);
+    insert(head, 6);
+    // createLoop(head);
+    head->next->next->next->next = head;
+    if (detectCycle(head))
+    {
+        cout << " cycle is found\n";
+    }
+    else
+    {
+        cout << "cycle is not found\n";
+    }
+
+    return 0;
+}
