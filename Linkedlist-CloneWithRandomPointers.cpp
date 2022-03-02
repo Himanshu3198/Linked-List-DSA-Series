2f37102e65fdb17cd911e43c62c09df683f7248d
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-03 01:13:05 +0530

    Update Linkedlist-CloneWithRandomPointers.cpp

--- Linkedlist-CloneWithRandomPointers.cpp
+++ Linkedlist-CloneWithRandomPointers.cpp
@@ -1,3 +1,5 @@
+// Time complexity O(n)
+// Space complexity O(n)
 /*
 // Definition for a Node.
 class Node {
@@ -18,59 +20,107 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
+        Node *newList=NULL;
+        Node *newHead=newList;
         
-        Node *copy,*front=head,*result;
-         
-         if(!head) 
-         {
-             return head;
-         }
-//          creating clone nodes
-        while(front){
-            
-            
-            copy=new Node(front->val);
-            
-            copy->next=front->next;
-            front->next=copy;
-            front=front->next->next;
-        }
-        result=head->next;
-        front=head;
-        //         connecting random pointers of clone node but note we losing the orginal link of linked list
-        while(front){
-            
-            if(front->random){
-                
-                front->next->random=front->random->next;
+        unordered_map<Node*,Node*>mp;
+        
+          Node *temp=head;
+        
+          while(temp){
               
-            }
-              front=front->next->next;
+              
+              Node *newNode=new Node(temp->val);
+               mp[temp]=newNode;
+              
+              temp=temp->next;
+          }
+        
+        temp=head;
+        
+        while(temp){
+            
+            mp[temp]->next=mp[temp->next];
+            mp[temp]->random=mp[temp->random];
+            
+            temp=temp->next;
         }
         
-//         final step reconnect the original link and clone list to next pointer
-        
-        
-        front=head;
-        while(front){
-            
-            copy=front->next;
-            front->next=copy->next;
-            
-            if(copy->next){
-                copy->next=front->next->next;
-                
-               
-            }
-             front=front->next;
-        }
-        
-        return result;
-        
-        
-        
+       return mp[head];
+    
+    }
+};
 
+//  time complexity O(n)
+//  space complexity O(1)
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+  
+       
+        Node *temp=head;
+        
+         Node *newList=NULL;
+        
+//          step 1 create new Nodes
+          while(temp){
+              
+              
+              Node *newNode=new Node(temp->val);
+              if(newList==NULL){
+                  newList=newNode;
+              }
+              
+              newNode->next=temp->next;
+              temp->next=newNode;
+              temp=newNode->next;
+          }
+        
+    // step 2 join random pointer
+        
+        temp=head;
+        
+        while(temp){
+            
+            temp->next->random=temp->random;
+            temp=temp->next->next;
+        }
+        
+        // step 3 join the newNodes
+        
+        temp=head;
+        
+        while(temp){
+            
+            Node *nextNode=temp->next->next;
+            Node *cloneNode=temp->next;
+            if(nextNode->next){
+                cloneNode->next=nextNode->next; 
+            }
+          else
+            temp=nextNode;
+        }
         
         
+        
+        return newList;
+    
     }
 };
