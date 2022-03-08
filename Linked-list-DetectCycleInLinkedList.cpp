8e9a49173954c2b1e593802b20799b3f48deaa3b
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-08 23:18:40 +0530

    Update Linked-list-DetectCycleInLinkedList.cpp

--- Linked-list-DetectCycleInLinkedList.cpp
+++ Linked-list-DetectCycleInLinkedList.cpp
@@ -1,65 +1,32 @@
 //  detect cycle in  a Linked List using c++
 //  Time Complexity O(n)  where n is no of nodes in linked list
 
-#include <iostream>
-using namespace std;
-
-struct Node
-{
-
-    struct Node *next;
-    int data;
-};
-
-void insert(Node *head, int value)
-{
-
-    struct Node *newnode = new Node;
-
-    newnode->data = value;
-    newnode->next = head;
-    head = newnode;
-}
-
-bool detectCycle(Node *head)
-{
-    Node *fast = head,*slow=head;
-     bool iscycle=false;
-    
-    while (fast->next != NULL && fast->next->next != NULL)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        if (fast == slow)
-        {
-            iscycle=true;
-            return iscycle;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        ListNode *fast=head;
+        ListNode *slow=head;
+        
+        
+        while(fast and fast->next){
+            
+            
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(fast==slow) return true;
         }
+        
+        return false;
     }
-      return iscycle;
-}
-
-int main()
-{
-    Node *head = NULL;
-
-    insert(head, 1);
-    insert(head, 2);
-    insert(head, 3);
-    insert(head, 4);
-    insert(head, 5);
-    insert(head, 6);
-    // createLoop(head);
-    head->next->next->next->next = head;
-    if (detectCycle(head))
-    {
-        cout << " cycle is found\n";
-    }
-    else
-    {
-        cout << "cycle is not found\n";
-    }
-
-    return 0;
-}
+};
   
