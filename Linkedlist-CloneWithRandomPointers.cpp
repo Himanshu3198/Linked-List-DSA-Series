c871e1aa490dbe26c43a5d68b84ba4a2e2422a18
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-12 22:42:39 +0530

    Update Linkedlist-CloneWithRandomPointers.cpp

--- Linkedlist-CloneWithRandomPointers.cpp
+++ Linkedlist-CloneWithRandomPointers.cpp
@@ -69,58 +69,73 @@ public:
 };
 */
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
-  
-       
+       if(!head) return NULL;
         Node *temp=head;
-        
-         Node *newList=NULL;
-        
-//          step 1 create new Nodes
-          while(temp){
-              
-              
-              Node *newNode=new Node(temp->val);
-              if(newList==NULL){
-                  newList=newNode;
-              }
-              
-              newNode->next=temp->next;
-              temp->next=newNode;
-              temp=newNode->next;
-          }
-        
-    // step 2 join random pointer
+        // create the newnodes
+        while(temp){
+            
+            Node *newNode=new Node(temp->val);
+            newNode->next=temp->next;
+            temp->next=newNode;
+            temp=newNode->next;
+        }
         
         temp=head;
         
+        // connecting  the random pointer
+        
+        
         while(temp){
-            
-            temp->next->random=temp->random;
+         // temp->random->next because it also should be random pointer not temp random
+            if(temp->random)
+            temp->next->random=temp->random->next;
             temp=temp->next->next;
         }
         
-        // step 3 join the newNodes
+        // connecting new nodes
+        
         
         temp=head;
+        Node *result=temp->next;
         
         while(temp){
             
-            Node *nextNode=temp->next->next;
-            Node *cloneNode=temp->next;
-            if(nextNode->next){
-                cloneNode->next=nextNode->next; 
-            }
-          else
-            temp=nextNode;
+            Node *copyNode=temp->next;
+            if(temp and temp->next)
+            temp->next=copyNode->next;
+            if(copyNode->next)
+            copyNode->next=temp->next->next;
+            temp=temp->next;
+            
         }
         
+        return result;
         
         
-        return newList;
-    
+        
+        
+        
+        
+  
     }
 };
